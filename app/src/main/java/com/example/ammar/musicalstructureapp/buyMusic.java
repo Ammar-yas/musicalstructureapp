32a6ee39927422f65a5ca1a0f5ab5b4bfbf94e5b
Ammar <plus.ammaryasser@gmail.vom>
2017-07-24 08:08:08 +0200

    final commit

--- app/src/main/java/com/example/ammar/musicalstructureapp/buyMusic.java
+++ app/src/main/java/com/example/ammar/musicalstructureapp/buyMusic.java
@@ -1,7 +1,10 @@
 package com.example.ammar.musicalstructureapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class buyMusic extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class buyMusic extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buy_music);
+        Button yourLibrary = (Button) findViewById(R.id.your_library_button);
+        yourLibrary.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(buyMusic.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
